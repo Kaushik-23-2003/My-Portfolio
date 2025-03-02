747d66b91e01a99abf03dcf4e06709892e7c78d3
Kaushik <104357488+Kaushik-23-2003@users.noreply.github.com>
2025-03-02 06:01:36 +0530

    Update site.ts

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,5 @@
-const site_url = "https://hariharan-kaushik.vercel.app/" || "https://kaushik-hariharan.vercel.app/";
+const site_url =
+  process.env.NEXT_PUBLIC_APP_URL || "https://the-portfolio-lac.vercel.app";
 
 export const siteConfig = {
   name: "Kaushik | Portfolio",
